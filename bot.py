e5bd9eab4534c40be36d4a7ef1aab6d9f352d67b
bebekk2024 <161502478+bebekk2024@users.noreply.github.com>
2024-04-03 02:10:26 +0700

    bot.py

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     except AccessTokenInvalid:
         raise Exception("Your BOT_TOKEN is not valid.")
     uname = app.get_me().username
-    print(f"@mhmdwldnnnn started successfully !")
+    print(f"@{uname} started successfully !")
     idle()
     app.stop()
     print("Bot stopped. Bye !")
